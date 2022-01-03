790a7cd771f0a644a9fd8511cbc97f712bf1e76d
AJS0191 <ashtonjamess@live.com>
2022-01-03 19:09:37 +0000

    adding parenthesis

--- 0x00-python-hello_world/0-print.py
+++ 0x00-python-hello_world/0-print.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Print the specified sting
-string = "Programming is like building a multilingual puzzle"
+string = "\"Programming is like building a multilingual puzzle"
 
 print(string)

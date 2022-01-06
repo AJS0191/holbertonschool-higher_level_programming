0584d947c66714f9d314ee3f405c6eab12392e06
AJS0191 <ashtonjamess@live.com>
2022-01-06 01:08:38 +0000

    fixing errors dealing with argv[0]

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -4,9 +4,9 @@
 def main():
     import sys
     argv = sys.argv
-    if len(argv) == 0:
+    if len(argv) - 1 == 0:
         print("0 arguments.")
-    elif len(argv) == 1:
+    elif len(argv) - 1 == 1:
         print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format((len(argv) - 1)))

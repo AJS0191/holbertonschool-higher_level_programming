98d5789b8697e9e1b394cf4287cdd5a2a88d0511
AJS0191 <ashtonjamess@live.com>
2022-01-06 01:05:19 +0000

    adding task 1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def main():
+    import sys
+    argv = sys.argv
+    if len(argv) == 0:
+        print("0 arguments.")
+    elif len(argv) == 1:
+        print("1 argument:\n1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format((len(argv) - 1)))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    main()

f45ebc19481752f3b7f413a821bda4cacc5ae910
AJS0191 <ashtonjamess@live.com>
2022-01-06 22:16:34 +0000

    adding 5; passes local

---
+++ 0x02-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    if my_string.count('c') == 0 and my_string.count('C') == 0:
+        return my_string
+    else:
+        new_string = ""
+        for i in range(my_string.count('c')):
+            for i in range(0, len(my_string)):
+                if my_string[i] != 'c' and my_string[i] != 'C':
+                    new_string = new_string + my_string[i]
+    return new_string

fa00c3ce24a3efd06d32ef035ee42b153ef8c0cc
AJS0191 <ashtonjamess@live.com>
2022-01-07 02:59:14 +0000

    pep

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 
 def divisible_by_2(my_list=[]):
     if my_list:
-        boolList = my_list.copy();
+        boolList = my_list.copy()
         for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
                 boolList[i] = True

0602158bc3bc03498c9b541237644d91ff36f8c2
AJS0191 <ashtonjamess@live.com>
2022-01-08 00:27:29 +0000

    pep

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 
 for i in range(0, 8):
-    for j in range (i, 9):
+    for j in range(i, 9):
         print("{}".format(i), end="")
-        print("{}".format(j + 1), end=", ")       
+        print("{}".format(j + 1), end=", ")
 print("89")

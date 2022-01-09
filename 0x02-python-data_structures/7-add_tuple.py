7c9cb5d4aa83481f3d60204682a9ad728a8745dd
AJS0191 <ashtonjamess@live.com>
2022-01-09 01:22:04 +0000

    pep

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         tuple_sum = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
@@ -10,7 +11,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1 and len(tuple_b) == 1:
         tuple_sum = (tuple_a[0] + tuple_b[0]), 0
     elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        tuple_sum = tuple_a[0] , tuple_a[1]
+        tuple_sum = tuple_a[0], tuple_a[1]
     elif len(tuple_a) == 0 and len(tuple_b) >= 2:
         tuple_sum = tuple_b[0],  tuple_b[1]
     elif len(tuple_a) == 1 and len(tuple_b) == 0:

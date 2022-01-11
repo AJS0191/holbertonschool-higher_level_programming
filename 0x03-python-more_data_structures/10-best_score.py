ce6a1cdac32e0cfd7f5ff1c6a292dd76ec0119b1
AJS0191 <ashtonjamess@live.com>
2022-01-11 04:22:40 +0000

    adding task 10; passes local

---
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary != None:
+        temp = []
+        for x, y in a_dictionary.items():
+            temp.append(y)
+        i = 0
+        for x, y in a_dictionary.items():
+            if y == sorted(temp)[-1]:
+                return x
+    else:
+        return None

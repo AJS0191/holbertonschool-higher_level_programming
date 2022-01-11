32e9a9c31bd0b59644ebdf2f911b9b8afb558d57
AJS0191 <ashtonjamess@live.com>
2022-01-11 18:30:12 +0000

    pep

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,7 +2,9 @@
 
 
 """Contains the square class"""
+
+
 class Square:
     """A square has two diminsions of the same size."""
     def __init__(self, size=0):
-        self.__size = size;
+        self.__size = size

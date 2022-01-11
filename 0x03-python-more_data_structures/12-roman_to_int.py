dd35d59431a3e891e714deaa3c0e20f17257e7eb
AJS0191 <ashtonjamess@live.com>
2022-01-11 06:32:04 +0000

    Adding task 12;passes local

---
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    romanN = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    thisN = list(map(lambda x: romanN.get(x), roman_string))
+    sumR = 0
+    if len(thisN) == 1:
+        return thisN[0]
+    for i in range(0, len(thisN)):
+        if i != 0:
+            if thisN[i] > thisN[i - 1]:
+                sumR = ((thisN[i]) - (thisN[i - 1] * 2)) + sumR
+            else:
+                sumR = thisN[i] + sumR
+        else: 
+            sumR = thisN[i] + sumR
+    return sumR

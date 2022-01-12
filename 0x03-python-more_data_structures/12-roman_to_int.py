8bf9359973e139d5728c2c6d82f85e3a611d05c9
AJS0191 <ashtonjamess@live.com>
2022-01-12 09:56:01 -0600

    change position None

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 
 
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     romanN = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     thisN = list(map(lambda x: romanN.get(x), roman_string))
     sumR = 0
-    if type(roman_string) is not str or roman_string is None:
-        return 0
     if len(thisN) == 1:
         return thisN[0]
     for i in range(0, len(thisN)):

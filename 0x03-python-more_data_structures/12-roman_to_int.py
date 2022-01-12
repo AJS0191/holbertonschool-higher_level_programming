880936d85e70e65954c81f0457ae1bcd12dc8b7d
AJS0191 <ashtonjamess@live.com>
2022-01-12 00:54:57 +0000

    pep and account for zero and wrong type

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,8 @@ def roman_to_int(roman_string):
     romanN = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     thisN = list(map(lambda x: romanN.get(x), roman_string))
     sumR = 0
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     if len(thisN) == 1:
         return thisN[0]
     for i in range(0, len(thisN)):
@@ -13,6 +15,6 @@ def roman_to_int(roman_string):
                 sumR = ((thisN[i]) - (thisN[i - 1] * 2)) + sumR
             else:
                 sumR = thisN[i] + sumR
-        else: 
+        else:
             sumR = thisN[i] + sumR
     return sumR

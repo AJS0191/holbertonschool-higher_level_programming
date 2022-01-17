10e71f073615bf4c88b22be1cb16264faba08d2f
AJS0191 <ashtonjamess@live.com>
2022-01-17 11:23:54 -0600

    doc

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """print the given name in a fun greeting"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:

c54764069832956fe5c26091d3b68b4d142fed29
AJS0191 <ashtonjamess@live.com>
2022-01-17 11:29:45 -0600

    pep8; different 0 result

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,16 +2,15 @@
 
 
 """Contains the print square fucntion"""
+
+
 def print_square(size):
     """Prints a square with the '#' character"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print("")
     for i in range(0, size):
         for k in range(0, size):
             print("#", end="")
         print("")
-    

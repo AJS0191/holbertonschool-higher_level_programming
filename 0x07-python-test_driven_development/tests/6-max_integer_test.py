b7fa9a868d7dddf4e0c853e6b3ecf8af30354afc
AJS0191 <ashtonjamess@live.com>
2022-01-17 12:09:38 -0600

    adding new tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,14 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         """test perfect case scenario"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max2(self):
+        """test perfect case scenario"""
+        self.assertEqual(max_integer([4, 2, 3, 0]), 4)
+
+    def test_max(self):
+        """test perfect case scenario"""
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
     
     def test_isempty(self):
         """test for when list is empty"""
@@ -18,6 +26,14 @@ class TestMaxInteger(unittest.TestCase):
     def test_containsNeg(self):
         """test for when list contains a negative"""
         self.assertEqual(max_integer([-5, 2, 3, 4]), 4)
+
+    def test_allNeg(self):
+        """test for when list is all negative"""
+        self.assertEqual(max_integer([-5, -2, -3, -4]), -2)
+
+    def test_oneItem(self):
+        """test for when list has one item"""
+        self.assertEqual(max_integer([4]), 4)
 
     def test_nonInt(self):
         """test for when list contains a non-int"""

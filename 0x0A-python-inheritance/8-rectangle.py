13cc9c55cba7f7b634ae362630158bb643061926
AJS0191 <ashtonjamess@live.com>
2022-01-20 21:17:02 -0600

    adding task 8; passes local

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module contains the Rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width__ = width
+        self.__height__ = height

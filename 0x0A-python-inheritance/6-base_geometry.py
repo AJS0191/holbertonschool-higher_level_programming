5136c32cc359fed074402ac078d129d076693beb
AJS0191 <ashtonjamess@live.com>
2022-01-20 19:19:00 -0600

    add task 6; passes local

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module contains the geometry class"""
+
+
+class BaseGeometry:
+    """the BaseGeometry class has the area method"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    

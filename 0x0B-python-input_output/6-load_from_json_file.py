3a0a07fe6af302aef7e940fe4f003305383b5cea
AJS0191 <ashtonjamess@live.com>
2022-01-21 20:52:18 -0600

    doc

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,6 @@ import json
 
 
 def load_from_json_file(filename):
+    """loads an object from a json type file"""
     with open(filename, 'r', encoding="utf-8") as fileA:
         return json.load(fileA)

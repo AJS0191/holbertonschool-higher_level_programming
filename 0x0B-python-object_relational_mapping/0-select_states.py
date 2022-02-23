9ccc719793b9663ac2badfcedd7b4e7469f65776
AJS0191 <ashtonjamess@live.com>
2022-02-23 13:47:04 -0600

    indent changes

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ import sys
 
 
 def main():
-        """lists states names and ids """
-        args = sys.argv
-        db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
-        cur = db.cursor()
-        cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close
-        db.close
+    """lists states names and ids """
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close
+    db.close
+
 
 if __name__ == "__main__":
     main()

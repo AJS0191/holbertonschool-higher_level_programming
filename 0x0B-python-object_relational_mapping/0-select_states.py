cb6f9d9060788f880610adef7567c2a06d8474fc
AJS0191 <ashtonjamess@live.com>
2022-02-23 13:59:26 -0600

    adding port

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """lists states names and ids """
     args = sys.argv
-    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=args[1], passwd=args[2], db=args[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
     rows = cur.fetchall()

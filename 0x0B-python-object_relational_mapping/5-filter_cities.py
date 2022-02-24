93a384226e08611424f43dff109b771c85040a0e
AJS9091 <ashtonjamess@live.com>
2022-02-24 08:59:32 -0800

    adding 5; passes local

---
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script will return states names and their ids"""
+import MySQLdb
+import sys
+
+
+def main():
+    """lists cities names in given state"""
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name, states.name FROM cities JOIN states\
+                 ON cities.state_id = states.id")
+    rows = cur.fetchall()
+    city_list = []
+    for row in rows:
+            if row[1] == args[4]:
+                city_list.append(row[0])
+    for i in range(0, len(city_list) - 1):
+        print("{}".format(city_list[i]), end=", ")
+    i += 1
+    print(city_list[i])
+    cur.close
+    db.close
+
+if __name__ == "__main__":
+    main()

f5ca38461af1778d08fa33dab0dc14640ec3444a
AJS9091 <ashtonjamess@live.com>
2022-02-25 08:03:06 -0800

    adding task 9

---
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module prints the names and id of states that contain """
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    """ This will print the instance.id and instance.name in table state """
+    args = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[1], args[2], args[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if 'a' in instance.name:
+            print("{}: {}".format(instance.id, instance.name))
+
+    session.close()

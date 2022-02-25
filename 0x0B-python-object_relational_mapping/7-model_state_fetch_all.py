8e623bddfb72b0de6f8289e8d054a1f33c51f7ab
AJS9091 <ashtonjamess@live.com>
2022-02-25 07:33:58 -0800

    just indentation!

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     for instance in session.query(State).order_by(State.id):
         print("{}: {}".format(instance.id, instance.name))
 
-session.close()
+    session.close()

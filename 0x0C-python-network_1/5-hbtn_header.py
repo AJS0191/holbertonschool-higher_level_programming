d53ae774d8f3c4b92b388bae14e4dbe7ab221a1a
AJS0191 <ashtonjamess@live.com>
2022-03-08 16:30:24 -0600

    fixing indentation

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -7,10 +7,10 @@ import sys
 
 def main():
     """takes a url, displays X-Request-Id"""
-args = sys.argv
-url = args[1]
-r = requests.head(url)
-print(r.headers.get('X-Request-Id'))
+    args = sys.argv
+    url = args[1]
+    r = requests.head(url)
+    print(r.headers.get('X-Request-Id'))
 
 
 if __name__ == '__main__':

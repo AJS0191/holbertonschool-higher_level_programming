8e0aa5b46f707bd67891b3eb3a8da388eeed3f3f
AJS0191 <ashtonjamess@live.com>
2022-03-08 10:06:33 -0600

    task 0; passes local

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""fetches a url and displays"""
+
+import urllib.request
+
+
+def main():
+    """fetches a url and displays"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        result = response.read()
+        utf = None
+    if 'charset=utf-8' in response.info()['Content-Type']:
+        utf = 'OK'
+    print("Body response:")
+    print(f"    - type: {type(result)}")
+    print(f"    - content: {result}")
+    print(f"    - utf8 content: {utf}")
+
+
+if __name__ == "__main__":
+    main()

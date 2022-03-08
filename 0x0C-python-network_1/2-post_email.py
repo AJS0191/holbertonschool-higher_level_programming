3034706b3fd882c88d9567a7d455c461055e8f32
AJS0191 <ashtonjamess@live.com>
2022-03-08 12:15:12 -0600

    task2; test in sandbox

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""passes an email to a url"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def main():
+    """passes an email to a url"""
+    args = sys.argv
+    url = args[1]
+    email = args[2]
+    values = {}
+    values['email'] = email
+    data = urllib.parse.urlencode(values)
+    data = data.encode(encoding='utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        result = response.read()
+    print(result)
+
+
+if __name__ == "__main__":
+    main()

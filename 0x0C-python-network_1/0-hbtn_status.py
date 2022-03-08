0776309fa72cf8a851e5f419caf5aeb25d2a3d08
AJS0191 <ashtonjamess@live.com>
2022-03-08 13:20:25 -0600

    use decode in place of for loop for utf8 detect

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -8,13 +8,10 @@ def main():
     """fetches a url and displays"""
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         result = response.read()
-        utf = None
-    if 'charset=utf-8' in response.info().get('Content-Type'):
-        utf = 'OK'
     print("Body response:")
     print(f"    - type: {type(result)}")
     print(f"    - content: {result}")
-    print(f"    - utf8 content: {utf}")
+    print(f"    - utf8 content: {result.decode('utf-8')}")
 
 
 if __name__ == "__main__":

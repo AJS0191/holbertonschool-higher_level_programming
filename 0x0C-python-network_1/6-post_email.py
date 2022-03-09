514e7304cdbbd5bdc0893a2382ef9a10f97cfc3d
AJS0191 <ashtonjamess@live.com>
2022-03-08 19:04:54 -0600

    task 6

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" sends an email to a url"""
+
+import requests
+import sys
+
+
+def main():
+    """sends an email to a url"""
+    args = sys.argv
+    url = args[1]
+    email = args[2]
+    payload = {'email': email}
+    r = requests.get(url, params=payload)
+    print(r.text)
+
+
+if __name__ == '__main__':
+    main()

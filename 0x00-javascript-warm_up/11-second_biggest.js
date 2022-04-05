eba2f4f759b704a68d7415fab656c8baa1f52a4f
AJS9091 <ashtonjamess@live.com>
2022-04-05 07:17:34 -0700

    dont print the array

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,4 @@ for (let i = 2; i < argvArray.length; i++) {
   givenArray.fill(argvArray[i], i - 2);
 }
 const sortMe = givenArray.sort();
-console.log(sortMe);
 console.log(sortMe[sortMe.length - 2]);

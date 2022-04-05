f69e8b2cd001f0d078a68f2f2c6120a8ec82b865
AJS0191 <ashtonjamess@live.com>
2022-04-05 09:48:33 -0500

    return numbers fix else

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -9,6 +9,6 @@ if (argvArray[2] === undefined) {
   for (let i = 2; i < argvArray.length; i++) {
     givenArray.fill(argvArray[i], i - 2);
   }
-}
 const sortMe = givenArray.sort();
-console.log(sortMe[sortMe.length - 2]);
+console.log(Number(sortMe[sortMe.length - 2]));
+}

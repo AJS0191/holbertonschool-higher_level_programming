88478258b76e5de97b6eabaadc9242d99a817178
AJS0191 <ashtonjamess@live.com>
2022-04-05 14:38:50 -0500

    adding print function to rectangle

---
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    if (this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let theString = '';
+        for (let x = 0; x < this.width; x++) {
+          theString += 'X';
+        }
+      console.log(theString);
+    }
+    }
+  }
+}
+module.exports = Rectangle;

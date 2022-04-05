a2b8d17a363d4be084f037184fbdfeaecdbebc53
AJS0191 <ashtonjamess@live.com>
2022-04-05 09:31:23 -0500

    adding lines for 1 and no values

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
 const argvArray = process.argv;
 const givenArray = new Array(argvArray.length - 2);
-for (let i = 2; i < argvArray.length; i++) {
-  givenArray.fill(argvArray[i], i - 2);
+if (argvArray[2] === undefined) {
+  console.log(0);
+} else if (argvArray[3] === undefined) {
+  console.log(0);
+} else {
+  for (let i = 2; i < argvArray.length; i++) {
+    givenArray.fill(argvArray[i], i - 2);
+  }
 }
 const sortMe = givenArray.sort();
 console.log(sortMe);

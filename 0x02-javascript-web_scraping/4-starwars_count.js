3ffaaa47009fcc73401546fdcaee6dddec123e02
AJS0191 <ashtonjamess@live.com>
2022-04-07 17:06:27 -0500

    revert

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,12 @@ const id = '18';
 let count = 0;
 request(argvArray[2], function (error, response, body) {
   const info = JSON.parse(body);
-  for (x in info.results) {
-    for (charc in info.results[x].characters) {
-      if (info.results[x].characters[charc] === 'https://swapi-api.hbtn.io/api/people/' + id + '/') {
-        count++;
-      }
+  for (let x in info.results) {
+    for (let charc in info.results[x].characters) {
+	if (info.results[x].characters[charc] === 'https://swapi-api.hbtn.io/api/people/' + id + '/') {
+            count++;
+	}
     }
   }
-  console.log(count);
-  x = 0;
-  charc = 0;
+    console.log(count);
 });

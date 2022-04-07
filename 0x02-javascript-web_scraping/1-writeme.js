5e7100e95623801bf1418bbedc81cf5ec5d56296
AJS0191 <ashtonjamess@live.com>
2022-04-07 14:00:27 -0500

    writes to files

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs')
+const argvArray = process.argv;
+
+fs.writeFile(argvArray[2],argvArray[3], err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+})

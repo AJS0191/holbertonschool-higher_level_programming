f357cd58d29238d1e1047cadeb32f16e6981a335
AJS9091 <ashtonjamess@live.com>
2022-04-07 13:09:02 -0700

    add an s

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const requests = require('request')
+const request = require('request')
 const argvArray = process.argv;
 
-requests(argvArray[2], function (error, response, body) {
+request(argvArray[2], function (error, response, body) {
   console.log('code: ' + response && response.statusCode);
 })

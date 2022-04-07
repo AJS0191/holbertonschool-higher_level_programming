4588986a23c6aba7bd97fa7a250ce0cb999ff65e
AJS9091 <ashtonjamess@live.com>
2022-04-07 14:48:12 -0700

    how many films is wedge in?

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const argvArray = process.argv;
+const id = '18';
+let count = 0;
+request(argvArray[2], function (error, response, body) {
+  const info = JSON.parse(body);
+  for (let x in info.results) {
+    for (let charc in info.results[x].characters) {
+	if (info.results[x].characters[charc] === 'https://swapi-api.hbtn.io/api/people/' + id + '/') {
+            count++;
+	}
+    }
+  }
+    console.log(count);
+});

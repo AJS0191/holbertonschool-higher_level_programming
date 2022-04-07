311a27803fd5a6aecec7d0095d47a69829edc3d1
AJS0191 <ashtonjamess@live.com>
2022-04-07 13:50:55 -0500

    readme.md and reading task

---
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs')
+const argvArray = process.argv;
+
+fs.readFile(argvArray[2],'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})

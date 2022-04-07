b1ebebf84c6eab21fe85be048053c1e5f930b8ee
AJS0191 <ashtonjamess@live.com>
2022-04-07 16:56:56 -0500

    semistandard

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,14 @@ const argvArray = process.argv;
 const id = '18';
 let count = 0;
 request(argvArray[2], function (error, response, body) {
+  console.error('error:', error);
   const info = JSON.parse(body);
-  for (let x in info.results) {
-    for (let charc in info.results[x].characters) {
-	if (info.results[x].characters[charc] === 'https://swapi-api.hbtn.io/api/people/' + id + '/') {
-            count++;
-	}
+  for (const x in info.results) {
+    for (const charc in info.results[x].characters) {
+      if (info.results[x].characters[charc] === 'https://swapi-api.hbtn.io/api/people/' + id + '/') {
+        count++;
+      }
     }
   }
-    console.log(count);
+  console.log(count);
 });
